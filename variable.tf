712f4978d9e81fecd3977461a4cdadafdd268e2f
Kumar1 <mohit.kumar1@softwareone.com>
2026-08-19 11:39:11 +0530

    Updated Variable.tf

--- variable.tf
+++ variable.tf
@@ -3,3 +3,8 @@ variable "aws_region" {
     type        = string
     default     = "us-east-1"
 }
+
+variable "aws_access_key" {
+    description = "The AWS access key"
+    type        = string
+}
